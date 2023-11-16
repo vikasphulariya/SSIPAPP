4b9196ec995da7bb5c70cc57323f344b2b4856f6
vikas Phulriya <vikasPhulariya@gmail.com>
2023-11-16 23:32:10 +0530

    SSIP APP

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import MainNavigator from './src/MainNavigator';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <MainNavigator />
+    </View>
+  );
+};
+
+export default App;
